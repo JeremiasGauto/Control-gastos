184065e4d2554db31632e6fa3c4cc8523e58939c
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-09-25 10:08:29 -0300

    componente de mensaje agregado

--- src/Components/NuevoPresupuesto.jsx
+++ src/Components/NuevoPresupuesto.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
+import Mensaje from './Mensaje';
 
 function NuevoPresupuesto({presupuesto, setPresupuesto}) {
-  return (
+    const [mensaje, setMensaje] = useState('');
+
+
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if (!Number(presupuesto) || Number(presupuesto) < 0 ) {
+            setMensaje('No es un presupuesto valido')
+        } else {
+            console.log('Si es un presupuesto valido')
+        }
+      
+  }
+  
+  
+  
+    return (
       <div className="contenedor-presupuesto contenedor sombra">
           
-          <form className='formulario'>
+          <form onSubmit={handlePresupuesto} className='formulario'>
               <div className='campo'>
                   <label htmlFor="">Definir Presupuesto</label>
 
@@ -18,7 +35,9 @@ function NuevoPresupuesto({presupuesto, setPresupuesto}) {
                   
                   />
               </div>
-              <input type="submit" value="Añadir"/>
+                <input type="submit" value="Añadir" />
+                
+                {mensaje && <Mensaje tipo ="error">{mensaje}</Mensaje>}
           </form>
       </div>
   )
