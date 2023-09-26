cfa85ed91811dbc86cb9468d7b9b19feddd91d27
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-09-26 08:41:25 -0300

    states agregado a los inputs de modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,72 +1,71 @@
-import cerrarBtn from '../assets/cerrar.svg'
-
+import { useState } from "react";
+import cerrarBtn from "../assets/cerrar.svg";
 
 function Modal({ setModal, animarModal, setAnimarModal }) {
+    const [nombre, setNombre] = useState('');
+    const [cantidad, setCantidad] = useState('');
+    const [categoria, setCategoria] = useState('');
 
-    const ocultarModal = () => {
+  const ocultarModal = () => {
+    setAnimarModal(false);
 
-        setAnimarModal(false);
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
 
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img src={cerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
+      </div>
 
-        setTimeout(() => {
-            setModal(false)
+      <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+        <legend>Nuevo Gasto</legend>
 
-        }, 500)
-    }
-
-    return (
-        <div className="modal">
-            <div className="cerrar-modal">
-                <img
-                    src={cerrarBtn}
-                    alt="cerrar modal"
-                    onClick={ocultarModal}
-                />
-            </div>
-
-            <form className={`formulario ${animarModal ? "animar" : "cerrar"}`} >
-                <legend>Nuevo Gasto</legend>
-
-                <div className='campo'>
-                    <label htmlFor="nombre">Nombre de gasto</label>
-                    <input
-                        id='nombre'
-                        type="text"
-                        placeholder='Añade el nombre del gasto'
-                    />
-                </div>
-                <div className='campo'>
-                    <label htmlFor="cantidad">Cantidad</label>
-                    <input
-                        id='cantidad'
-                        type="number"
-                        placeholder='Añade la cantidad  del gasto ej:300'
-                    />
-                </div>
-                <div className='campo'>
-                    <label htmlFor="categoria">Categoria</label>
-                    <select id="categoria">
-                        <option value="">--Seleccione--</option>
-                        <option value="Ahorro">Ahorro</option>
-                        <option value="Comida">Comida</option>
-                        <option value="Casa">Casa</option>
-                        <option value="Gastos varios">Gastos varios</option>
-                        <option value="Ocio">Ocio</option>
-                        <option value="Salud">Salud</option>
-                        <option value="Suscripciones">Suscripciones</option>
-
-                    </select>
-
-                </div>
-
-                <input
-                    type="submit"
-                    value="Añadir"
-                />
-
-            </form>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre de gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el nombre del gasto"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
         </div>
-    )
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade la cantidad  del gasto ej:300"
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            <option value="">--Seleccione--</option>
+            <option value="Ahorro">Ahorro</option>
+            <option value="Comida">Comida</option>
+            <option value="Casa">Casa</option>
+            <option value="Gastos varios">Gastos varios</option>
+            <option value="Ocio">Ocio</option>
+            <option value="Salud">Salud</option>
+            <option value="Suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir" />
+      </form>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
